e134fb28db5a4429f8de56379754aadcf95411be
DaMetin <mj.strobl@hotmail.com>
2025-12-03 17:03:08 +0100

    Added improved_euler

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -55,6 +55,26 @@ namespace ASC_ode
     }
   };
 
+  class ImprovedEuler : public TimeStepper
+{
+  Vector<> m_vecf, m_vecf2, m_y_temp;
+public:
+  ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
+  : TimeStepper(rhs), m_vecf(rhs->dimF()), m_vecf2(rhs->dimF()), m_y_temp(rhs->dimX()) {}
+  
+  void doStep(double tau, VectorView<double> y) override
+  {
+    // Erster Schritt: k1 = f(y_n)
+    this->m_rhs->evaluate(y, m_vecf);
+    
+    // Zweiter Schritt: k2 = f(y_n + tau * k1)
+    m_y_temp = y + tau * m_vecf;
+    this->m_rhs->evaluate(m_y_temp, m_vecf2);
+    
+    // Update: y_{n+1} = y_n + tau/2 * (k1 + k2)
+    y += (tau / 2.0) * (m_vecf + m_vecf2);
+  }
+};
 
   
 
