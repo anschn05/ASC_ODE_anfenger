899c5463275a9a18f48f09a0876db2c9ad48b3cc
DaMetin <mj.strobl@hotmail.com>
2025-12-03 18:05:22 +0100

    JackNicoloson

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -55,26 +55,47 @@ namespace ASC_ode
     }
   };
 
-  class ImprovedEuler : public TimeStepper
-{
-  Vector<> m_vecf, m_vecf2, m_y_temp;
-public:
-  ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
-  : TimeStepper(rhs), m_vecf(rhs->dimF()), m_vecf2(rhs->dimF()), m_y_temp(rhs->dimX()) {}
-  
-  void doStep(double tau, VectorView<double> y) override
+ class ImprovedEuler : public TimeStepper
   {
-    // Erster Schritt: k1 = f(y_n)
-    this->m_rhs->evaluate(y, m_vecf);
-    
-    // Zweiter Schritt: k2 = f(y_n + tau * k1)
-    m_y_temp = y + tau * m_vecf;
-    this->m_rhs->evaluate(m_y_temp, m_vecf2);
-    
-    // Update: y_{n+1} = y_n + tau/2 * (k1 + k2)
-    y += (tau / 2.0) * (m_vecf + m_vecf2);
-  }
-};
+    Vector<> m_vecf;
+  public:
+    ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_vecf(rhs->dimF()) {}
+
+
+    void doStep(double tau, VectorView<double> y) override
+    {
+      this->m_rhs->evaluate(y, m_vecf);
+      Vector<double> y_welle = y + tau/2 * m_vecf;
+
+      this->m_rhs->evaluate(y_welle, m_vecf);
+      y += tau * m_vecf;
+    }
+  };
+
+   class CrankNicolson : public TimeStepper
+  {
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0)) 
+    {
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      auto f_old = Compose(m_rhs, m_yold);
+      auto f_new = m_rhs;
+      m_equ = ynew - m_yold - (0.5 * (m_tau * (f_old + f_new)));
+    }
+
+    void doStep(double tau, VectorView<double> y) override
+    {
+      m_yold->set(y);
+      m_tau->set(tau);
+      NewtonSolver(m_equ, y);
+    }
+  };
 
   
 
