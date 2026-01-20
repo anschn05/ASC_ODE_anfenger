58a826c25558a44595442f70d6b4d803041fcbcd
ManuueellXD <emanuel.d.steininger@gmail.com>
2026-01-20 21:37:48 +0100

    plotmassspring, test_mass_spring angepasst, kreisel aufrecht, kamera gedreht

--- demos/plotmassspring.py
+++ demos/plotmassspring.py
@@ -1,23 +1,21 @@
 import numpy as np
-data = np.loadtxt('output_test_ode.txt', usecols=(0, 1, 2))
-# print (data)
+data = np.loadtxt('output_mass_spring_py.txt', usecols=(0, 1, 2))
 
 import matplotlib.pyplot as plt
 
-plt.plot(data[:,0], data[:,1], label='position')
-plt.plot(data[:,0], data[:,2], label='velocity')
+plt.plot(data[:,0], data[:,1], label='pos z (Mass A)')
+plt.plot(data[:,0], data[:,2], label='vel z (Mass A)')
 plt.xlabel('time')
 plt.ylabel('value')
-plt.title('Mass-Spring System Time Evolution')
+plt.title('Mass-Spring (Python) Time Evolution')
 plt.legend()
 plt.grid()
 plt.show()
 
-
-plt.plot(data[:,1], data[:,2], label='phase plot')
-plt.xlabel('position')
-plt.ylabel('velocity')
-plt.title('Mass-Spring System Phase Plot')
+plt.plot(data[:,1], data[:,2], label='phase plot (z)')
+plt.xlabel('position z')
+plt.ylabel('velocity z')
+plt.title('Mass-Spring (Python) Phase Plot')
 plt.legend()
 plt.grid()
 plt.show()
--- mechsystem/test_mass_spring.py
+++ mechsystem/test_mass_spring.py
@@ -1,6 +1,6 @@
-import sys
-sys.path.append('/Users/joachim/texjs/lva/IntroSC/ASC-ODE/build/mechsystem')
-sys.path.append('../build/mechsystem')
+import os, sys
+here = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(os.path.join(here, "..", "build", "mechsystem")))
 
 from mass_spring import *
 
@@ -8,25 +8,27 @@ from mass_spring import *
 mss = MassSpringSystem3d()
 mss.gravity = (0,0,-9.81)
 
-mA = mss.add (Mass(1, (1,0,0)))
-mB = mss.add (Mass(2, (2,0,0)))
+mA = mss.add (Mass(1, (0,0,1)))
+mB = mss.add (Mass(2, (0.2,0,2)))
 f1 = mss.add (Fix( (0,0,0)) )
 mss.add (Spring(1, 10, (f1, mA)))
 mss.add (Spring(1, 20, (mA, mB)))
-
+A = mss.masses[0].pos
+B = mss.masses[1].pos
+L0 = ((B[0]-A[0])**2 + (B[1]-A[1])**2 + (B[2]-A[2])**2)**0.5   
 # Add distance constraint with Lagrange multipliers
-constraint_AB = DistanceConstraint(mA, mB, 1.5)
+constraint_AB = DistanceConstraint(mA, mB, L0)
 mss.add_constraint(constraint_AB)
 
 
 print ("state = ", mss.getState())
 
-mss.simulate (0.1, 10)
+mss.simulate (0.01, 100)
 
 print ("state = ", mss.getState())
 
 
-mss.simulate (0.1, 10)
+mss.simulate (0.01, 100)
 
 print ("state = ", mss.getState())
 
@@ -37,3 +39,20 @@ mss.masses[0].mass = 5
 
 for m in mss.masses:
     print (m.mass, m.pos)
+
+dt = 0.001
+nsteps = 20000
+
+with open("output_mass_spring_py.txt", "w") as f:
+    for k in range(nsteps+1):
+        t = k*dt
+
+        A = mss.masses[0]
+        # falls vel verfügbar:
+        vz = A.vel[2] if hasattr(A, "vel") else 0.0
+
+        f.write(f"{t} {A.pos[2]} {vz}\n")
+
+        if k < nsteps:
+            mss.simulate(dt, 1)
+print("Simulation complete, data written to output_mass_spring_py.txt")
